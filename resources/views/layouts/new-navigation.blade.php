69eee63426447d7ade0ece3cb2961f1cffbfca9f
ishfak-akbar <ishfakakbar24@gmail.com>
2026-01-22 16:47:56 +0600

    changed navigation bar background color

--- resources/views/layouts/new-navigation.blade.php
+++ resources/views/layouts/new-navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="text-white shadow-lg" style="background: #109884;">
+<nav x-data="{ open: false }" class="text-white shadow-lg" style="background: #0bb098;">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="header-container h-16">
             <!-- Left Side: LedgerMate App Name -->
